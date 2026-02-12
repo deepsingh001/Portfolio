e36e0f77900f930bc4a7b9c4ad20bab931c8acc4
macre <macre@DESKTOP-TIUCGKV>
2026-02-12 08:55:32 +0530

    chnages

--- managementsystem.client/src/app/home/home.component.ts
+++ managementsystem.client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit } from '@angular/core';
+
+declare var AOS: any;
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+
+ngAfterViewInit(): void {
+    if (AOS) {
+      AOS.init({
+        once: true,
+        duration: 800
+      });
+    }
+  }
 
 }
+
+
+// import { Component, AfterViewInit } from '@angular/core';
+
+// declare var AOS: any;
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements AfterViewInit {
+
+//   ngAfterViewInit(): void {
+//     if (AOS) {
+//       AOS.init({
+//         once: true,
+//         duration: 800
+//       });
+//     }
+//   }
+
+// }
