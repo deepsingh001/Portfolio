a961f4d89d031ab9b707106c91bad047a12194cf
macre <macre@DESKTOP-TIUCGKV>
2026-02-12 23:12:27 +0530

    sdfas

--- managementsystem.client/src/app/contact/contact.component.ts
+++ managementsystem.client/src/app/contact/contact.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var AOS: any;
 @Component({
   selector: 'app-contact',
   standalone: false,
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
 
+
+export class ContactComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+   formData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  submitted = false;
+
+  handleSubmit(event: Event) {
+    event.preventDefault();
+
+    console.log(this.formData);
+    this.submitted = true;
+
+    setTimeout(() => {
+      this.submitted = false;
+      this.formData = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      };
+    }, 3000);
+  }
 }
